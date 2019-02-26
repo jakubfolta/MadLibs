d239116cc68f7574a32ad1bae6db1743a8e81fca
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-26 21:57:03 +0100

    first commit

---
+++ madLibs.py
@@ -0,0 +1,13 @@
+#! python3
+
+#Mad Libs
+
+import sys
+import pyperclip
+import shelve
+
+# TODO: Open new file to save new changed text.
+# TODO: Read file.
+# TODO: Search for specific words while looping and replacing
+# them with user input.
+# TODO: Print new text and save it to a new file.
