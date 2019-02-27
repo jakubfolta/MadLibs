6e0970b0e6f33c36a2865fd85a37b0be97975b47
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-27 10:15:40 +0100

    continuation

--- madLibs.py
+++ madLibs.py
@@ -8,14 +8,17 @@ import shelve
 
 # Open text file to read and a new file to save new changed text.
 text_file = open('textfile.txt')
-print(text_file.read())
-mltext = open('new_text.txt', 'w')
 text_file_r = text_file.read()
+splitted_text = text_file_r.split()
+print(text_file_r)
+print(splitted_text)
+mltext = open('new_text.txt', 'w')
+
 
 # TODO: Search for specific words while looping and replacing.
-# them with user input.
-#for x in :
-
+#   them with user input.
+#for x in : 
+    
 
 # TODO: Print new text and save it to a new file.
 
