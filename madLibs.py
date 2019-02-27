6480a805948d554fa265bf72766e46ed7d3dd1b5
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-27 09:32:39 +0100

    continuation

--- madLibs.py
+++ madLibs.py
@@ -6,8 +6,11 @@ import sys
 import pyperclip
 import shelve
 
-# TODO: Open new file to save new changed text.
-# TODO: Read file.
+# TODO: Open text file to read and a new file to save new changed text.
+text_file = open('textfile.txt')
+print(text_file.read())
+mltext = open('new_text.txt', 'w')
+
 # TODO: Search for specific words while looping and replacing
 # them with user input.
 # TODO: Print new text and save it to a new file.
