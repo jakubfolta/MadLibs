36818b5b52c6a658c9632ecac2110edd04904648
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-27 18:28:25 +0100

    continuation

--- madLibs.py
+++ madLibs.py
@@ -4,7 +4,6 @@
 
 import sys
 import pyperclip
-import shelve
 
 # Open text file to read and a new file to save new changed text.
 text_file = open('textfile.txt')
@@ -16,16 +15,20 @@ print(file_content)
 print(splitted_text)
 
 # TODO: Search for specific words while looping and replacing.
-#   them with user input.
-for x in splitted_text :
-    print(x)
-    mltext.write(x)
-    if x == 'ADJECTIVE':
+# them with user input.
+for index, x in enumerate(splitted_text):
+    if x == splitted_text[len(splitted_text) - 1]:
+        mltext.write(x)
+    elif x == 'ADJECTIVE':
         print('Enter an adjective:')
+        mltext.write(x.replace(x, input()))
+        print(x)
+    else:
+        mltext.write(x + ' ')
 mltext.close()
 mltext = open('new_text.txt')
 print(mltext.read())
-        
+    
     
 
 # TODO: Print new text and save it to a new file.
