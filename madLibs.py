5dc8053c843fefe0805fab8ec206010b9c22cbee
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-28 16:36:25 +0100

    continuation

--- madLibs.py
+++ madLibs.py
@@ -19,36 +19,21 @@ print(splitted_text)
 # them with user input.
 for index, x in enumerate(splitted_text):
     print(index)
-    if x.startswith(('ADJECTIVE', 'NOUN', 'ADVERB', 'VERB')):
-        if x == 'ADJECTIVE':
+        if x.startswith('ADJECTIVE'):
             print('Enter an adjective:')
             mltext.write(x.replace(x, input()), + ' ')
-        elif x == 'NOUN':
+        elif x.startswith('NOUN'):
             print('Enter a noun:')
             mltext.write(x.replace(x, input()), + ' ')
-        elif x == 'ADVERB':
+        elif x.startswith('ADVERB'):
             print('Enter an adverb:')
             mltext.write(x.replace(x, input()), + ' ')
-        elif x == 'VERB':
+        elif x.startswith('VERB'):
             print('Enter a verb:')
             mltext.write(x.replace(x, input()), + ' ')
-        mltext.write(x + ' ')
-            
-        mltext.write(x + ' ')
-    elif x.startswith(('ADJECTIVE', 'NOUN', 'ADVERB', 'VERB')):
-        mltext.write(x)
-    elif not x.startswith(('ADJECTIVE', 'NOUN', 'ADVERB', 'VERB')) and not index == (len(splitted_text) - 1):
-        mltext.write(x + ' ')
-    else:
-        mltext.write(x)
-    '''elif x == 'ADJECTIVE':
-        if x == splitted_text[len(splitted_text) - 1]:
-            print('Enter an adjective:')
-            mltext.write(x.replace(x, input()))
         else:
-            print('Enter an adjective:')
-            mltext.write(x.replace(x, input() + ' '))
-            print(x)'''
+            mltext.write(x + ' ')
+    
     
 mltext.close()
 mltext = open('new_text.txt')
