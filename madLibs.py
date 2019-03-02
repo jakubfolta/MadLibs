1be693af1fa522b58bee8fe7f70ad0c4de5e8f05
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-02 16:31:36 +0100

    continuation

--- madLibs.py
+++ madLibs.py
@@ -1,43 +1,44 @@
 #! python3
 
-#Mad Libs
+#Mad Libs - A program which replace specified words with user inputs, save it to a new file and prints it to the console. 
 
-import sys
-import pyperclip
+import re
 
+end_space_regex = re.compile(r'')
 # Open text file to read and a new file to save new changed text.
 text_file = open('textfile.txt', 'r+').read()
 mltext = open('new_text.txt', 'r+')
 print(text_file)
 splitted_text = text_file.split()
 print(splitted_text)
-# TODO: Search for specific words while looping and replacing.
-# them with user input.
+
+
+# Search for specific words while looping, replace them with user input and save new text to a new file.
 for x in splitted_text:
     if x.startswith('ADJECTIVE'):
         print('Enter an adjective:')
-        mltext.write(x.replace('ADJECTIVE', input() + ''))
+        mltext.write(x.replace('ADJECTIVE', input()))
+        mltext.write(' ')
     elif x.startswith('NOUN'):
         print('Enter a noun:')
-        mltext.write(x.replace('NOUN', input() + ''))
+        mltext.write(x.replace('NOUN', input()))
+        mltext.write(' ')
     elif x.startswith('ADVERB'):
         print('Enter an adverb:')
-        mltext.write(x.replace('ADVERB', input() + ''))
+        mltext.write(x.replace('ADVERB', input()))
+        mltext.write(' ')
     elif x.startswith('VERB'):
         print('Enter a verb:')
-        mltext.write(x.replace('VERB', input() + ''))
+        mltext.write(x.replace('VERB', input()))
+        mltext.write(' ')
     else:
         mltext.write(x + ' ')
-
-    
-    
+        
 mltext.close()
-mltext = open('new_text.txt').read()
-print(mltext)
-    
 
 
 # TODO: Print new text and save it to a new file.
+print(open('new_text.txt').read())
 
 
 # TODO: Change project status to "finished".
